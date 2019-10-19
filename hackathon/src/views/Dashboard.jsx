4045b441dec66a4d90662f86b7b6e667c3a1c01b
kamsi09 <kamsioduche85@gmail.com>
2019-10-18 23:28:34 -0500

    Refactor Landing Page

--- hackathon/src/views/Dashboard.jsx
+++ hackathon/src/views/Dashboard.jsx
@@ -74,32 +74,6 @@ class Dashboard extends React.Component {
                   <Row>
                     <Col md="4" xs="5">
                       <div className="icon-big text-center icon-warning">
-                        <i className="nc-icon nc-money-coins text-success" />
-                      </div>
-                    </Col>
-                    <Col md="8" xs="7">
-                      <div className="numbers">
-                        <p className="card-category">Revenue</p>
-                        <CardTitle tag="p">$ 1,345</CardTitle>
-                        <p />
-                      </div>
-                    </Col>
-                  </Row>
-                </CardBody>
-                <CardFooter>
-                  <hr />
-                  <div className="stats">
-                    <i className="far fa-calendar" /> Last day
-                  </div>
-                </CardFooter>
-              </Card>
-            </Col>
-            <Col lg="3" md="6" sm="6">
-              <Card className="card-stats">
-                <CardBody>
-                  <Row>
-                    <Col md="4" xs="5">
-                      <div className="icon-big text-center icon-warning">
                         <i className="nc-icon nc-vector text-danger" />
                       </div>
                     </Col>
